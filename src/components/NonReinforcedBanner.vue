0a69d60b86ad47acf3d2f44010599a7ab09c15db
Derrick Gall <derrickorama@gmail.com>
2023-04-16 16:47:41 -0500

    Fixed display of NonReinforcedBanner - added keep level

--- src/components/NonReinforcedBanner.vue
+++ src/components/NonReinforcedBanner.vue
@@ -2,7 +2,7 @@
   <NegativeBanner v-if="nonReinforcedPlayers.length" centered>
     <div>{{ nonReinforcedPlayers.length }} players are not reinforced</div>
     <ul>
-      <li v-for="player in nonReinforcedPlayers">{{ player.name }}</li>
+      <li v-for="player in nonReinforcedPlayers">{{ player.name }} k.{{ player.keepLevel }}</li>
     </ul>
   </NegativeBanner>
 </template>
@@ -10,6 +10,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { usePlayerStore } from 'stores/player';
+import NegativeBanner from 'components/NegativeBanner.vue';
 
 const playerStore = usePlayerStore();
 const { nonReinforcedPlayers } = storeToRefs(playerStore);
