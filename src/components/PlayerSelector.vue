5c10789d2dcc56d3f091797a0bdcb25bbbbfb614
Derrick Gall <derrickorama@gmail.com>
2023-05-06 18:24:33 -0500

    Excluding keeps that are >= the reinforcer level - from the reinforce dropdown

--- src/components/PlayerSelector.vue
+++ src/components/PlayerSelector.vue
@@ -21,13 +21,37 @@ const props = defineProps<{ playerId: string; showSelect: boolean; }>();
 
 const emit = defineEmits(['select']);
 
-const { groupView, nonReinforcedPlayers } = storeToRefs(usePlayerStore());
+const { groupView, nonReinforcedPlayers, players } = storeToRefs(usePlayerStore());
+
+const player = computed(() => players.value[props.playerId]);
 
 const playerOptions = computed(() => {
-  const players = nonReinforcedPlayers.value.filter(({ id }) => id !== props.playerId);
+  const players = nonReinforcedPlayers.value.filter(({ id, keepLevel }) => {
+    if (id === props.playerId) {
+      // Do not include themselves
+      return false;
+    }
+    if (keepLevel >= player.value.keepLevel) {
+      // Do not include keeps that are >= the reinforcer level
+      return false;
+    }
+    return true;
+  })
+
+  // Sort by keep level
+  players.sort((a, b) => {
+    if (a.keepLevel > b.keepLevel) {
+      return -1;
+    } else if (b.keepLevel > a.keepLevel) {
+      return 1;
+    }
+    return 0;
+  })
+
   if (groupView.value === 'early') {
     return players.filter(({ isInEarlyGroup }) => isInEarlyGroup)
   }
+
   return players;
 });
 
